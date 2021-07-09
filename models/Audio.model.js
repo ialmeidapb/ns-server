1ff6d81d19978410cdd3e77502ecc54ca2e12c65
ialmeida <ialmeida.pb@gmail.com>
2021-07-09 12:10:40 +0100

    models done

---
+++ models/Audio.model.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const AudioSchema = mongoose.Schema({
+  created_by: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  date_of_upload: { type: Date, default: Date.now },
+  media: [{ type: String, required: true, trim: true }],
+  title: { type: String, required: true, trim: true },
+
+});
+
+module.exports = mongoose.model("PatientRecord", AudioSchema);
